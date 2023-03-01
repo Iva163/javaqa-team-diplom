892d1468f2f8640d2feda14cc1ebdc97e44122fc
Shuranova N <ni.shuranova@gmail.com>
2023-03-01 21:40:11 +0300

    commit add tests

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
-    @Test
+    @Test // проверяет наличие игр в каталоге
     public void shouldAddGame() {
 
         GameStore store = new GameStore();
@@ -17,7 +17,7 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game2));
     }
 
-    @Test
+    @Test // регистрирует количество времени сыгранное в одном каталоге
     public void MustAddApPlayingTime() {
 
         GameStore store = new GameStore();
@@ -31,7 +31,7 @@ public class GameStoreTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @Test // выбирает игрока с наибольшим временем наигранных часов
     public void ShouldFindPlayerWithLongestPlayedTime() {
         GameStore store = new GameStore();
 
@@ -45,7 +45,8 @@ public class GameStoreTest {
 
     }
 
-    @Test
+
+    @Test //регистрирует количество времени сыгранное в одном каталоге
     public void MustRegisterPlayingTime() {
         GameStore store = new GameStore();
 
@@ -60,6 +61,31 @@ public class GameStoreTest {
 
 
     }
+    @Test
+    public void shouldGetTitle() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        String expected = "Нетология Баттл Онлайн";
+        String actual = game.getTitle();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddGameNotExist() {
 
+        GameStore store = new GameStore();
+        Game game1 = null;
+        Game game2 = store.publishGame("Баттл", "Шутер");
+
+        assertFalse(store.containsGame(game1));
+    }
+    @Test
+    public void shouldEqualsGame() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Баттл", "Шутер");
+        Game game2 = store.publishGame("Баттл", "Шутер");
+
+        assertEquals(game1, game2);
+    }
 
 }
